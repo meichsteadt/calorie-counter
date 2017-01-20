61ff21012158d7f10427690737fd9958a265e2af
Matt Eichsteadt <matt@>
2017-01-19 19:19:37 -0800

    start pipeline functionality

--- app/entry-list.component.ts
+++ app/entry-list.component.ts
@@ -4,33 +4,48 @@ import { Entry } from './entry.model';
 @Component({
   selector: 'entry-list',
   template: `
-  <div class="entries">
-    <table class="table table-bordered table-striped">
-      <thead>
-      <tr>
-        <th>Name</th>
-        <th>Details</th>
-        <th>Calories</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr *ngFor="let entry of entries">
-        <td>{{entry.name}}</td>
-        <td>{{entry.details}}</td>
-        <td>{{entry.calories}}</td>
-      </tr>
-      <tr class="warning">
-        <td colspan="2"><strong>Total</strong></td>
-        <td>{{sum(entries)}}</td>
-      </tr>
-      </tbody>
-    </table>
+  <div class="row">
+    <select class="form-control" (change)='filter($event.target.value)'>
+      <option value="" disabled selected>Choose your option</option>
+      <option value="all">All</option>
+      <option value="low">Low</option>
+      <option value="high">High</option>
+    </select>
   </div>
+  <div class="row">
+    <div class="entries">
+      <table class="table table-bordered table-striped">
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>Details</th>
+          <th>Calories</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr *ngFor="let entry of entries | calories:level">
+          <td>{{entry.name}}</td>
+          <td>{{entry.details}}</td>
+          <td>{{entry.calories}}</td>
+        </tr>
+        <tr class="warning">
+          <td colspan="2"><strong>Total</strong></td>
+          <td>{{sum(entries)}}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
   `
 })
 
 export class EntryListComponent {
   @Input() entries: Entry[];
+  level: number = 100000000000;
+  filter(level) {
+    ;
+  }
 
   sum(entries: Entry[]) {
     let sum: number = 0;
